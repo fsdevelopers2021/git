d49273f2d40a13a98836127a85d5583b55ae4f6b
vasud <vasud@LAPTOP-UP40LL45>
2021-06-19 21:45:08 +0530

    HelloWorldTest java file commit

---
+++ src/com/fsdevelopers/test/HelloWorldTest.java
@@ -0,0 +1,8 @@
+package com.fsdevelopers.test;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+           System.out.println("Hey this is Hello World Program in java...");
+	}
+}
